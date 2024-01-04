ada4bee7a54f65a2226c0e16dfa063ab946f5b3b
Seth Hobson <sethlawrencehobson@gmail.com>
2024-01-04 17:57:03 -0500

    added put/:code route

--- routes/companies.js
+++ routes/companies.js
@@ -52,7 +52,37 @@ router.post("/", async function (req, res) {
   const company = results.rows[0];
 
   return (res.json({ company }));
+});
+
+/** PUT / -
+ * {name, description} returns {company: {code, name, description}}*/
+
+router.put('/:code', async function (req, res) {
+  if (req.body === undefined) {
+    throw new BadRequestError();
+  }
+
+  const code = req.params.code;
+  const results = await db.query(
+    `UPDATE companies
+        SET name = $2,
+        description = $3
+        WHERE code = $1
+        RETURNING code, name, description`,
+    [code, req.body.name, req.body.description]
+  );
+
+  const company = results.rows[0];
+
+  if (!company) {
+    throw new NotFoundError(`${company} not found`);
+  }
+
+  return (res.json({ company }));
 })
+
+
+
 
 
 module.exports = router;
