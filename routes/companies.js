40a892ef0c9a20d812d2491da5e8d5f8008cc242
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-01-04 12:30:30 -0800

    added post route for company

--- routes/companies.js
+++ routes/companies.js
@@ -6,7 +6,8 @@ const express = require("express");
 
 const router = new express.Router();
 const db = require("../db");
-const { NotFoundError } = require('../expressError');
+const { NotFoundError, BadRequestError } = require('../expressError');
+
 /** GET / - returns {companies: [{code, name}, ...]} */
 
 router.get("/", async function (req, res) {
@@ -16,20 +17,42 @@ router.get("/", async function (req, res) {
   return res.json({ companies });
 });
 
+/** GET /[code] - returns {company: {code, name, description}} */
+
 router.get('/:code', async function (req, res) {
-  const code = req.params.id;
+  const code = req.params.code;
   const results = await db.query(
-    'SELECT id, FROM biztime WHERE id = $1', [id]
+    'SELECT code, name, description FROM companies WHERE code = $1', [code]
   );
 
   const company = results.rows[0];
 
   if (company === undefined) {
-    throw new NotFoundError(`no matching company: ${id}`);
+    throw new NotFoundError(`no matching company: ${code}`);
   }
 
   return (res.json({ company }));
 });
+
+/** POST / -
+ * {code, name, description} returns {company: {code, name, description}}*/
+
+router.post("/", async function (req, res) {
+  if (req.body === undefined) {
+    throw new BadRequestError();
+  }
+
+  const results = await db.query(
+    `INSERT INTO companies (code, name, description)
+      VALUES ($1, $2, $3)
+      RETURNING code, name, description`,
+    [req.body.code, req.body.name, req.body.description]
+  );
+
+  const company = results.rows[0];
+
+  return (res.json({ company }));
+})
 
 
 module.exports = router;
