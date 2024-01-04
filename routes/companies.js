1b902663f693f8427e23dab85fc30a861661a081
Seth Hobson <sethlawrencehobson@gmail.com>
2024-01-04 15:10:56 -0500

    added get code route

--- routes/companies.js
+++ routes/companies.js
@@ -6,7 +6,7 @@ const express = require("express");
 
 const router = new express.Router();
 const db = require("../db");
-
+const { NotFoundError } = require('../expressError');
 /** GET / - returns {companies: [{code, name}, ...]} */
 
 router.get("/", async function (req, res) {
@@ -14,7 +14,22 @@ router.get("/", async function (req, res) {
   const companies = results.rows;
 
   return res.json({ companies });
-})
+});
+
+router.get('/:code', async function (req, res) {
+  const code = req.params.id;
+  const results = await db.query(
+    'SELECT id, FROM biztime WHERE id = $1', [id]
+  );
+
+  const company = results.rows[0];
+
+  if (company === undefined) {
+    throw new NotFoundError(`no matching company: ${id}`);
+  }
+
+  return (res.json({ company }));
+});
 
 
 module.exports = router;
